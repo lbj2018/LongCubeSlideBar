912171902d0f38d94fd362afa5b1a7787d5c676a
dengfeng <dengfeng@cfountain.com>
2015-08-18 15:18:57 +0800

    Initial Commit

---
+++ LongCubeSlideBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LongCubeSlideBar
+//
+//  Created by zhou dengfeng derek on 18/8/15.
+//  Copyright (c) 2015 Creative Fountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LongCubeSlideBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LongCubeSlideBar
+//
+//  Created by zhou dengfeng derek on 18/8/15.
+//  Copyright (c) 2015 Creative Fountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
